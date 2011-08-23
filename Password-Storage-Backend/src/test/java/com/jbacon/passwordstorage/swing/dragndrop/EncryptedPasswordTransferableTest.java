7083349dde220f1dfa1146e8b6fc386b6c0717e1
James Bacon <cheese1989@gmail.com>
2011-08-23 21:43:27 +0100

    Renamed test for transferable encrypted password and created test for DataFlavour code.

--- Password-Storage-Backend/src/test/java/com/jbacon/passwordstorage/swing/dragndrop/EncryptedPasswordTransferableTest.java
+++ Password-Storage-Backend/src/test/java/com/jbacon/passwordstorage/swing/dragndrop/EncryptedPasswordTransferableTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TransferableEncryptedPasswordTest {
+public class EncryptedPasswordTransferableTest {
 
     @Test
     public void testTransferableEncryptedPassword() {
---
+++ Password-Storage-Backend/src/test/java/com/jbacon/passwordstorage/swing/dragndrop/EncryptionPasswordDataFlavourTest.java
@@ -0,0 +1,14 @@
+package com.jbacon.passwordstorage.swing.dragndrop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncryptionPasswordDataFlavourTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
