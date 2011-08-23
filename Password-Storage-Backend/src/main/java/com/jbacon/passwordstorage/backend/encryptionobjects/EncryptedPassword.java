97dc9dfc0253e891d157980bcb2ffd69c54570dc
James Bacon <cheese1989@gmail.com>
2011-08-23 20:40:57 +0100

    Added code to deal with Drag & Drop functionality

--- Password-Storage-Backend/src/main/java/com/jbacon/passwordstorage/backend/encryptionobjects/EncryptedPassword.java
+++ Password-Storage-Backend/src/main/java/com/jbacon/passwordstorage/backend/encryptionobjects/EncryptedPassword.java
@@ -1,59 +1,118 @@
 package com.jbacon.passwordstorage.backend.encryptionobjects;
 
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.Serializable;
 
-public class EncryptedPassword implements Serializable {
+//Added in transferable code for drag and drop
+public class EncryptedPassword implements Serializable, Transferable, ClipboardOwner {
 
-	private static final long serialVersionUID = -1839898907563182564L;
+    private static final long serialVersionUID = -1839898907563182564L;
+    public static DataFlavor encryptedPasswordFlavor = null;
+    public static DataFlavor localEncryptedPasswordFlavor = null;
 
-	private String encryptedPasswordName;
-	private String encryptedPassword;
-	private String encryptedWebsiteUrl;
-	private PasswordType passwordType;
-	private String profileName;
+    static {
+        try {
+            encryptedPasswordFlavor = new DataFlavor(EncryptedPassword.class, "Non Local EncryptedPassword");
+            localEncryptedPasswordFlavor = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + "; class=EncryptedPassword", "Local EncryptedPassword");
+        } catch (Exception e) {
+        }
+    }
 
-	public String getEncryptedPasswordName() {
-		return encryptedPasswordName;
-	}
+    private String encryptedPasswordName;
+    private String encryptedPassword;
+    private String encryptedWebsiteUrl;
+    private PasswordType passwordType;
+    private String profileName;
 
-	public String getProfileName() {
-		return profileName;
-	}
+    public EncryptedPassword(final String encryptedPasswordName, final String profileName, final String encryptedPassword, final PasswordType passwordType,
+            final String encryptedWebsiteURL) {
+        this.encryptedPasswordName = encryptedPasswordName;
+        this.profileName = profileName;
+        this.encryptedPassword = encryptedPassword;
+        this.passwordType = passwordType;
 
-	public String getEncryptedPassword() {
-		return encryptedPassword;
-	}
+        if (passwordType == PasswordType.WEBSITE) {
+            this.encryptedWebsiteUrl = encryptedWebsiteURL;
+        } else {
+            this.encryptedWebsiteUrl = "N/A";
+        }
+    }
 
-	public PasswordType getPasswordType() {
-		return passwordType;
-	}
+    public String getEncryptedPasswordName() {
+        return encryptedPasswordName;
+    }
 
-	public String getEncryptedWebsiteURL() {
-		return encryptedWebsiteUrl;
-	}
+    public String getProfileName() {
+        return profileName;
+    }
 
-	public void setEncryptedPasswordName(final String encryptedPasswordName) {
-		this.encryptedPasswordName = encryptedPasswordName;
-	}
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
 
-	public void setProfileName(final String profileName) {
-		this.profileName = profileName;
-	}
+    public PasswordType getPasswordType() {
+        return passwordType;
+    }
 
-	public void setEncryptedPassword(final String encryptedPassword) {
-		this.encryptedPassword = encryptedPassword;
-	}
+    public String getEncryptedWebsiteURL() {
+        return encryptedWebsiteUrl;
+    }
 
-	public void setPasswordType(final PasswordType passwordType) {
-		this.passwordType = passwordType;
-	}
+    public void setEncryptedPasswordName(final String encryptedPasswordName) {
+        this.encryptedPasswordName = encryptedPasswordName;
+    }
 
-	public void setEncryptedWebsiteURL(final String encryptedWebsiteUrl) {
-		this.encryptedWebsiteUrl = encryptedWebsiteUrl;
-	}
+    public void setProfileName(final String profileName) {
+        this.profileName = profileName;
+    }
 
-	@Override
-	public String toString() {
-		return "Name: " + encryptedPasswordName + ", Type: " + passwordType + ", URL: " + encryptedWebsiteUrl;
-	}
+    public void setEncryptedPassword(final String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public void setPasswordType(final PasswordType passwordType) {
+        this.passwordType = passwordType;
+        if (passwordType != null && passwordType != PasswordType.WEBSITE) {
+            this.encryptedWebsiteUrl = "N/A";
+        }
+    }
+
+    public void setEncryptedWebsiteURL(final String encryptedWebsiteUrl) {
+        this.encryptedWebsiteUrl = encryptedWebsiteUrl;
+    }
+
+    @Override
+    public DataFlavor[] getTransferDataFlavors() {
+        return new DataFlavor[] { encryptedPasswordFlavor, localEncryptedPasswordFlavor };
+    }
+
+    @Override
+    public boolean isDataFlavorSupported(final DataFlavor flavor) {
+        if (flavor.equals(encryptedPasswordFlavor) || flavor.equals(localEncryptedPasswordFlavor)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object getTransferData(final DataFlavor flavor) throws UnsupportedFlavorException {
+        if (flavor.equals(encryptedPasswordFlavor) || flavor.equals(localEncryptedPasswordFlavor)) {
+            return this;
+        }
+        throw new UnsupportedFlavorException(flavor);
+    }
+
+    @Override
+    public void lostOwnership(final Clipboard clipboard, final Transferable contents) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + encryptedPasswordName + ", Type: " + passwordType + ", URL: " + encryptedWebsiteUrl;
+    }
 }
