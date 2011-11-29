8f15769d1e04d12adb1e117d90e4f0eb8665e0d0
James Bacon <james.bacon@technophobia.com>
2011-11-29 13:11:22 +0000

    Updated the last unimplemeted password generators to use the AbstractPasswordGenerator.

--- password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/PhrasePasswordGenerator.java
+++ password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/PhrasePasswordGenerator.java
@@ -1,5 +1,17 @@
 package com.jbacon.passwordstorage.password.generation;
 
-public class PhrasePasswordGenerator implements PasswordGenerator {
+public class PhrasePasswordGenerator extends AbstractPasswordGenerator {
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword(final PasswordGeneratorProperty... properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/UnicodePasswordGenerator.java
+++ password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/UnicodePasswordGenerator.java
@@ -1,5 +1,17 @@
 package com.jbacon.passwordstorage.password.generation;
 
-public class UnicodePasswordGenerator implements PasswordGenerator {
+public class UnicodePasswordGenerator extends AbstractPasswordGenerator {
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword(final PasswordGeneratorProperty... properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
