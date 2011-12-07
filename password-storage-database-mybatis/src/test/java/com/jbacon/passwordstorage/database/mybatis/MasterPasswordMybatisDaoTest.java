676e7b9a1707a0ed4e2bfa3dce63a5e6ab40d5aa
James Bacon <james.bacon@technophobia.com>
2011-12-07 13:33:27 +0000

    Started to Implement tests for the MasterPasswordMybatisDao

--- password-storage-database-mybatis/src/test/java/com/jbacon/passwordstorage/database/mybatis/MasterPasswordMybatisDaoTest.java
+++ password-storage-database-mybatis/src/test/java/com/jbacon/passwordstorage/database/mybatis/MasterPasswordMybatisDaoTest.java
@@ -1,26 +1,62 @@
 package com.jbacon.passwordstorage.database.mybatis;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.jbacon.passwordstorage.database.mybatis.MasterPasswordMybatisDao;
+import com.jbacon.passwordstorage.password.MasterPassword;
 import com.jbacon.test.tools.RemoveTestFiles;
 
 public class MasterPasswordMybatisDaoTest {
 
+	private static final String TEST_SQLITE_DATABASE_FILENAME = "dbTest.sqlite";
+	private static final String TEST_MYBATIS_CONFIGURATION_FILENAME = "mybatisTest/Configuration.xml";
+
+	private static final String TEST_VALUE_SALT = "salt";
+	private static final String TEST_VALUE_ENCRYPTED_SECRET_KEY = "encryptedSecretKey";
+	private static final String TEST_VALUE_PROFILE_NAME = "profileName";
+
+	private static MaintenanceMybatisDao MAINTENANCE_DAO;
+	private static MasterPasswordMybatisDao MASTER_PASSWORD_DAO;
+
 	@AfterClass
 	public static void cleanUp() throws IOException {
-		RemoveTestFiles.remove("dbTest.sqlite");
+		RemoveTestFiles.remove(TEST_SQLITE_DATABASE_FILENAME);
 	}
 
-	MasterPasswordMybatisDao dao;
+	private static void createTables() throws IOException {
+		MAINTENANCE_DAO = new MaintenanceMybatisDao(TEST_MYBATIS_CONFIGURATION_FILENAME);
+		MAINTENANCE_DAO.createMasterPasswordTable();
+	}
 
-	public MasterPasswordMybatisDaoTest() throws IOException {
-		dao = new MasterPasswordMybatisDao("mybatisTest/Configuration.xml");
+	private static void dropTables() throws IOException {
+		MAINTENANCE_DAO = new MaintenanceMybatisDao(TEST_MYBATIS_CONFIGURATION_FILENAME);
+		MAINTENANCE_DAO.dropMasterPasswordTable();
+	}
+
+	private static MasterPassword generateMasterPassword(final int id) {
+		return new MasterPassword(TEST_VALUE_PROFILE_NAME + id, TEST_VALUE_ENCRYPTED_SECRET_KEY + id, TEST_VALUE_SALT + id, null, null, null);
+	}
+
+	@BeforeClass
+	public static void setup() throws IOException {
+		dropTables();
+		createTables();
+
+		MASTER_PASSWORD_DAO = new MasterPasswordMybatisDao(TEST_MYBATIS_CONFIGURATION_FILENAME);
+	}
+
+	@Test
+	public void shouldBeAbleToInstertMasterPassword() {
+		Integer result = MASTER_PASSWORD_DAO.instertMasterPassword(generateMasterPassword(1));
+		assertThat(result, is(equalTo(1)));
 	}
 
 	@Test
@@ -44,13 +80,7 @@ public class MasterPasswordMybatisDaoTest {
 	}
 
 	@Test
-	public void testInstertMasterPassword() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testUpdateMasterPassword() {
 		fail("Not yet implemented");
 	}
-
 }
