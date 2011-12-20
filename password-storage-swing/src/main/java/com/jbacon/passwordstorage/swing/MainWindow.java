f2a337e19ee592b29ecbdcdfb4de4748ba9f380b
James Bacon <james.bacon@technophobia.com>
2011-12-20 13:24:37 +0000

    Removed "SwingFactory" as it was useless.
    Continued development of the Swing GUI.

--- password-storage-swing/src/main/java/com/jbacon/passwordstorage/swing/MainWindow.java
+++ password-storage-swing/src/main/java/com/jbacon/passwordstorage/swing/MainWindow.java
@@ -2,6 +2,7 @@ package com.jbacon.passwordstorage.swing;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -11,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.sql.Timestamp;
 import java.util.GregorianCalendar;
 
+import javax.swing.AbstractButton;
 import javax.swing.AbstractListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
@@ -34,16 +36,13 @@ import com.jbacon.passwordstorage.swing.table.StoredPasswordTableModel;
 
 public class MainWindow {
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(final String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				try {
 					MainWindow window = new MainWindow();
-					window.frmPasswordBox.setVisible(true);
+					window.mainWindowJFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -51,16 +50,27 @@ public class MainWindow {
 		});
 	}
 
-	private JFrame frmPasswordBox;
+	private static void printMessage(final String message) {
+		System.out.println(message);
+	}
+
+	private JFrame mainWindowJFrame;
+
 	private StoredPasswordTableModel storedPasswordsModel;
+
 	private JTable storedPasswordsJTable;
+
+	private JList availableProfilesJList;
+
+	private JScrollPane storedPasswordsJScrollPane;
 
 	private JMenuBar menuBar;
 	private JMenu mnFile;
 	private JMenu mnEdit;
 	private JMenu mnView;
+
 	private JMenu mnHelp;
-	private JMenuItem mntmOpenProfile;
+	private JMenuItem mntmLoadProfile;
 	private JMenuItem mntmNewProfile;
 	private JMenuItem mntmNewPassword;
 	private JMenuItem mntmExit;
@@ -70,60 +80,148 @@ public class MainWindow {
 	private JMenuItem mntmFaq;
 	private JMenuItem mntmHelpContents;
 	private JMenuItem mntmOpenWikiPage;
+
 	private JMenuItem mntmOpenDownloadPage;
-
-	private JSeparator separator_3;
-	private JSeparator separator_4;
-	private JSeparator separator;
-
-	private JCheckBoxMenuItem chckbxmntmToggleSidebar;
+	private JSeparator firstFileMenuJSeparator;
+	private JSeparator secondFileMenuJSeparator;
+	private JSeparator secondHelpMenuJSeparator;
+	private JSeparator firstHelpMenuJSeparator;
+	private JSeparator thirdHelpMenuJSeparator;
 	private JSeparator viewMenuSeparator;
+
+	private JSeparator availableProfilesButtonSeparator;
+	private JCheckBoxMenuItem chckbxmntmToggleSidebar;
 	private JCheckBoxMenuItem chckbxmntmToggleActionButtons;
+
 	private JCheckBoxMenuItem chckbxmntmToggleAvailableProfiles;
-
 	private JPanel westJPanel;
+	private JPanel centreJPanel;
+
 	private JPanel availableProfilesNorthButtonJPanel;
+	private JButton newProfileJButton;
+	private JButton loadProfileJButton;
+	private JButton deleteProfileJButton;
+	private JButton newPasswordJBbutton;
+	private JButton openPasswordJButton;
+	private JButton deletePasswordJButton;
+	private JMenuItem mntmDeleteProfile;
+	private JMenuItem mntmOpenPassword;
 
-	private JList availableProfilesJList;
+	private JMenuItem mntmDeletePassword;
 
-	/**
-	 * Create the application.
-	 */
 	public MainWindow() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	private void areEnabled(final boolean isEnabled, final Component... components) {
+		for (Component component : components) {
+			component.setEnabled(isEnabled);
+		}
+	}
+
+	private void areSelected(final boolean isSelected, final AbstractButton... components) {
+		for (AbstractButton button : components) {
+			button.setSelected(isSelected);
+		}
+	}
+
+	private void areVisible(final boolean isVisible, final Component... components) {
+		for (Component component : components) {
+			component.setVisible(isVisible);
+		}
+	}
+
+	private void deletePassword() {
+		printMessage("deletePassword");
+	}
+
+	private void deleteProfile() {
+		printMessage("deleteProfile");
+	}
+
+	private void exitProgram() {
+		System.exit(0);
+	}
+
 	private void initialize() {
-		frmPasswordBox = new JFrame();
-		frmPasswordBox.setTitle("Password Box");
-		frmPasswordBox.setBounds(100, 100, 800, 600);
-		frmPasswordBox.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindowJFrame = new JFrame();
+		mainWindowJFrame.setTitle("Password Box");
+		mainWindowJFrame.setBounds(100, 100, 800, 600);
+		mainWindowJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		menuBar = new JMenuBar();
-		frmPasswordBox.setJMenuBar(menuBar);
+		mainWindowJFrame.setJMenuBar(menuBar);
 
 		mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 
-		mntmOpenProfile = new JMenuItem("Open Profile");
-		mnFile.add(mntmOpenProfile);
-
 		mntmNewProfile = new JMenuItem("New Profile");
+		mntmNewProfile.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				newProfile();
+			}
+		});
 		mnFile.add(mntmNewProfile);
 
-		JSeparator separator_1 = new JSeparator();
-		mnFile.add(separator_1);
+		mntmLoadProfile = new JMenuItem("Load Profile");
+		mntmLoadProfile.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				loadProfile();
+			}
+		});
+		mnFile.add(mntmLoadProfile);
+
+		mntmDeleteProfile = new JMenuItem("Delete Profile");
+		mntmDeleteProfile.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				deleteProfile();
+			}
+		});
+		mnFile.add(mntmDeleteProfile);
+
+		firstFileMenuJSeparator = new JSeparator();
+		mnFile.add(firstFileMenuJSeparator);
 
 		mntmNewPassword = new JMenuItem("New Password");
+		mntmNewPassword.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				newPassword();
+			}
+		});
 		mnFile.add(mntmNewPassword);
 
-		JSeparator separator_2 = new JSeparator();
-		mnFile.add(separator_2);
+		mntmOpenPassword = new JMenuItem("Open Password");
+		mntmOpenPassword.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				openPassword();
+			}
+		});
+		mnFile.add(mntmOpenPassword);
+
+		mntmDeletePassword = new JMenuItem("Delete Password");
+		mntmDeletePassword.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				deletePassword();
+			}
+		});
+		mnFile.add(mntmDeletePassword);
+
+		secondFileMenuJSeparator = new JSeparator();
+		mnFile.add(secondFileMenuJSeparator);
 
 		mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				exitProgram();
+			}
+		});
 		mnFile.add(mntmExit);
 
 		mnEdit = new JMenu("Edit");
@@ -140,9 +238,13 @@ public class MainWindow {
 		chckbxmntmToggleSidebar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(final ActionEvent evt) {
-				westJPanel.setVisible(chckbxmntmToggleSidebar.isSelected());
-				chckbxmntmToggleActionButtons.setEnabled(chckbxmntmToggleSidebar.isSelected());
-				chckbxmntmToggleAvailableProfiles.setEnabled(chckbxmntmToggleSidebar.isSelected());
+				boolean isSelected = chckbxmntmToggleSidebar.isSelected();
+				areVisible(isSelected, westJPanel);
+				areEnabled(isSelected, chckbxmntmToggleActionButtons, chckbxmntmToggleAvailableProfiles);
+				if (isSelected) {
+					areSelected(isSelected, chckbxmntmToggleActionButtons, chckbxmntmToggleAvailableProfiles);
+					areVisible(isSelected, availableProfilesNorthButtonJPanel, availableProfilesJList);
+				}
 			}
 		});
 		mnView.add(chckbxmntmToggleSidebar);
@@ -155,7 +257,7 @@ public class MainWindow {
 		chckbxmntmToggleActionButtons.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(final ActionEvent evt) {
-				availableProfilesNorthButtonJPanel.setVisible(chckbxmntmToggleActionButtons.isSelected());
+				areVisible(chckbxmntmToggleActionButtons.isSelected(), availableProfilesNorthButtonJPanel);
 			}
 		});
 		mnView.add(chckbxmntmToggleActionButtons);
@@ -165,7 +267,7 @@ public class MainWindow {
 		chckbxmntmToggleAvailableProfiles.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(final ActionEvent evt) {
-				availableProfilesJList.setVisible(chckbxmntmToggleAvailableProfiles.isSelected());
+				areVisible(chckbxmntmToggleAvailableProfiles.isSelected(), availableProfilesJList);
 			}
 		});
 		mnView.add(chckbxmntmToggleAvailableProfiles);
@@ -176,8 +278,8 @@ public class MainWindow {
 		mntmCheckForUpdates = new JMenuItem("Check For Updates");
 		mnHelp.add(mntmCheckForUpdates);
 
-		separator_4 = new JSeparator();
-		mnHelp.add(separator_4);
+		firstHelpMenuJSeparator = new JSeparator();
+		mnHelp.add(firstHelpMenuJSeparator);
 
 		mntmOpenWikiPage = new JMenuItem("Open Wiki Page");
 		mnHelp.add(mntmOpenWikiPage);
@@ -185,8 +287,8 @@ public class MainWindow {
 		mntmOpenDownloadPage = new JMenuItem("Open Download Page");
 		mnHelp.add(mntmOpenDownloadPage);
 
-		separator_3 = new JSeparator();
-		mnHelp.add(separator_3);
+		secondHelpMenuJSeparator = new JSeparator();
+		mnHelp.add(secondHelpMenuJSeparator);
 
 		mntmFaq = new JMenuItem("F.A.Q.");
 		mnHelp.add(mntmFaq);
@@ -194,16 +296,16 @@ public class MainWindow {
 		mntmHelpContents = new JMenuItem("Help Contents");
 		mnHelp.add(mntmHelpContents);
 
-		separator = new JSeparator();
-		mnHelp.add(separator);
+		thirdHelpMenuJSeparator = new JSeparator();
+		mnHelp.add(thirdHelpMenuJSeparator);
 
 		mntmAbout = new JMenuItem("About Password Box");
 		mnHelp.add(mntmAbout);
-		frmPasswordBox.getContentPane().setLayout(new BorderLayout(0, 0));
+		mainWindowJFrame.getContentPane().setLayout(new BorderLayout(0, 0));
 
 		westJPanel = new JPanel();
 		westJPanel.setBackground(Color.WHITE);
-		frmPasswordBox.getContentPane().add(westJPanel, BorderLayout.WEST);
+		mainWindowJFrame.getContentPane().add(westJPanel, BorderLayout.WEST);
 		westJPanel.setLayout(new BorderLayout(0, 0));
 
 		availableProfilesNorthButtonJPanel = new JPanel();
@@ -217,7 +319,13 @@ public class MainWindow {
 		gbl_availableProfilesNorthButtonJPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		availableProfilesNorthButtonJPanel.setLayout(gbl_availableProfilesNorthButtonJPanel);
 
-		JButton newProfileJButton = new JButton("New Profile");
+		newProfileJButton = new JButton("New Profile");
+		newProfileJButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				newProfile();
+			}
+		});
 		GridBagConstraints gbc_newProfileJButton = new GridBagConstraints();
 		gbc_newProfileJButton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_newProfileJButton.insets = new Insets(0, 0, 5, 0);
@@ -225,7 +333,13 @@ public class MainWindow {
 		gbc_newProfileJButton.gridy = 0;
 		availableProfilesNorthButtonJPanel.add(newProfileJButton, gbc_newProfileJButton);
 
-		JButton loadProfileJButton = new JButton("Load Profile");
+		loadProfileJButton = new JButton("Load Profile");
+		loadProfileJButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				loadProfile();
+			}
+		});
 		GridBagConstraints gbc_loadProfileJButton = new GridBagConstraints();
 		gbc_loadProfileJButton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_loadProfileJButton.insets = new Insets(0, 0, 5, 0);
@@ -233,7 +347,13 @@ public class MainWindow {
 		gbc_loadProfileJButton.gridy = 1;
 		availableProfilesNorthButtonJPanel.add(loadProfileJButton, gbc_loadProfileJButton);
 
-		JButton deleteProfileJButton = new JButton("Delete Profile");
+		deleteProfileJButton = new JButton("Delete Profile");
+		deleteProfileJButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				deleteProfile();
+			}
+		});
 		GridBagConstraints gbc_deleteProfileJButton = new GridBagConstraints();
 		gbc_deleteProfileJButton.insets = new Insets(0, 0, 5, 0);
 		gbc_deleteProfileJButton.fill = GridBagConstraints.HORIZONTAL;
@@ -241,7 +361,7 @@ public class MainWindow {
 		gbc_deleteProfileJButton.gridy = 2;
 		availableProfilesNorthButtonJPanel.add(deleteProfileJButton, gbc_deleteProfileJButton);
 
-		JSeparator availableProfilesButtonSeparator = new JSeparator();
+		availableProfilesButtonSeparator = new JSeparator();
 		availableProfilesButtonSeparator.setForeground(Color.BLACK);
 		GridBagConstraints gbc_availableProfilesButtonSeparator = new GridBagConstraints();
 		gbc_availableProfilesButtonSeparator.fill = GridBagConstraints.BOTH;
@@ -250,7 +370,13 @@ public class MainWindow {
 		gbc_availableProfilesButtonSeparator.gridy = 3;
 		availableProfilesNorthButtonJPanel.add(availableProfilesButtonSeparator, gbc_availableProfilesButtonSeparator);
 
-		JButton newPasswordJBbutton = new JButton("New Password");
+		newPasswordJBbutton = new JButton("New Password");
+		newPasswordJBbutton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				newPassword();
+			}
+		});
 		GridBagConstraints gbc_newPasswordJBbutton = new GridBagConstraints();
 		gbc_newPasswordJBbutton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_newPasswordJBbutton.insets = new Insets(0, 0, 5, 0);
@@ -258,7 +384,13 @@ public class MainWindow {
 		gbc_newPasswordJBbutton.gridy = 4;
 		availableProfilesNorthButtonJPanel.add(newPasswordJBbutton, gbc_newPasswordJBbutton);
 
-		JButton openPasswordJButton = new JButton("Open Password");
+		openPasswordJButton = new JButton("Open Password");
+		openPasswordJButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				openPassword();
+			}
+		});
 		GridBagConstraints gbc_openPasswordJButton = new GridBagConstraints();
 		gbc_openPasswordJButton.insets = new Insets(0, 0, 5, 0);
 		gbc_openPasswordJButton.fill = GridBagConstraints.HORIZONTAL;
@@ -266,7 +398,13 @@ public class MainWindow {
 		gbc_openPasswordJButton.gridy = 5;
 		availableProfilesNorthButtonJPanel.add(openPasswordJButton, gbc_openPasswordJButton);
 
-		JButton deletePasswordJButton = new JButton("Delete Password");
+		deletePasswordJButton = new JButton("Delete Password");
+		deletePasswordJButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				deletePassword();
+			}
+		});
 		GridBagConstraints gbc_deletePasswordJButton = new GridBagConstraints();
 		gbc_deletePasswordJButton.gridx = 0;
 		gbc_deletePasswordJButton.gridy = 6;
@@ -289,12 +427,13 @@ public class MainWindow {
 		availableProfilesJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		availableProfilesJList.setBorder(new CompoundBorder(new EmptyBorder(10, 5, 5, 5), new TitledBorder(new LineBorder(new Color(184, 207, 229)),
 				"Available Profiles", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(51, 51, 51))));
+		availableProfilesJList.setPreferredSize(new java.awt.Dimension(165, 0));
 		westJPanel.add(availableProfilesJList, BorderLayout.CENTER);
 
-		JPanel centreJPanel = new JPanel();
+		centreJPanel = new JPanel();
 		centreJPanel.setBackground(Color.WHITE);
 		centreJPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		frmPasswordBox.getContentPane().add(centreJPanel, BorderLayout.CENTER);
+		mainWindowJFrame.getContentPane().add(centreJPanel, BorderLayout.CENTER);
 
 		storedPasswordsModel = new StoredPasswordTableModel();
 		storedPasswordsModel.add(new StoredPassword("encryptedPasswordName", "profileName", "encryptedPassword", "encryptedPasswordNotes", new Timestamp(
@@ -310,10 +449,32 @@ public class MainWindow {
 
 		centreJPanel.setLayout(new BorderLayout(0, 0));
 
-		JScrollPane storedPasswordsJScrollPane = new JScrollPane();
+		storedPasswordsJScrollPane = new JScrollPane();
 		centreJPanel.add(storedPasswordsJScrollPane, BorderLayout.CENTER);
 
-		storedPasswordsJTable = SwingComponantFactory.createPasswordJTable(storedPasswordsModel);
+		storedPasswordsJTable = new JTable();
+		storedPasswordsJTable.setFillsViewportHeight(true);
+		storedPasswordsJTable.setModel(storedPasswordsModel);
+		storedPasswordsJTable.setColumnSelectionAllowed(false);
+		storedPasswordsJTable.setCellSelectionEnabled(false);
+		storedPasswordsJTable.setRowSelectionAllowed(true);
+		storedPasswordsJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		storedPasswordsJScrollPane.setViewportView(storedPasswordsJTable);
+	}
+
+	private void loadProfile() {
+		printMessage("loadProfile");
+	}
+
+	private void newPassword() {
+		printMessage("newPassword");
+	}
+
+	private void newProfile() {
+		printMessage("newProfile");
+	}
+
+	private void openPassword() {
+		printMessage("openPassword");
 	}
 }
