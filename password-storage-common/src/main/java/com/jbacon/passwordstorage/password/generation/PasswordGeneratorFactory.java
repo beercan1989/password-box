60dd53ac061e542d16e72acbab02483871780a6d
James Bacon <james.bacon@technophobia.com>
2012-06-12 13:11:58 +0100

    Added and updated some javadocs.

--- password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/PasswordGeneratorFactory.java
+++ password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/PasswordGeneratorFactory.java
@@ -10,66 +10,111 @@ import static com.jbacon.passwordstorage.password.generation.CharSet.add;
 import static com.jbacon.passwordstorage.password.generation.CharSet.concat;
 
 public enum PasswordGeneratorFactory {
-    
-    LETTER {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(LOWER_ALPHA.getChars());
-        }
-    },
-    
-    NUMBER {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(NUMERIC.getChars());
-        }
-    },
-    
-    CAPITAL {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(UPPER_ALPHA.getChars());
-        }
-    },
-    
-    LETTER_NUMBER {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(concat(LOWER_ALPHA, NUMERIC));
-        }
-    },
-    
-    LETTER_CAPITAL {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(MIXED_ALPHA.getChars());
-        }
-    },
-    
-    CAPITAL_NUMBER {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(concat(NUMERIC, UPPER_ALPHA));
-        }
-    },
-    
-    LETTER_CAPITAL_NUMBER {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(MIXED_ALPHA_NUMERIC.getChars());
-        }
-    },
-    
-    LETTER_NUMBER_PUNCTUATION {
-        @Override
-        public PasswordGenerator getPasswordGenerator() {
-            return new PasswordGeneratorImpl(concat(LOWER_ALPHA, NUMERIC, SPECIAL));
-        }
-    };
-    
-    public static PasswordGenerator getCustomPasswordGenerator(final char[] chars, final char... extraChars) {
-        return new PasswordGeneratorImpl(add(chars, extraChars));
-    }
-    
-    public abstract PasswordGenerator getPasswordGenerator();
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with just lower case letters in the character set.
+	 */
+	LETTER {
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(LOWER_ALPHA.getChars());
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with just numbers in the character set.
+	 */
+	NUMBER {
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(NUMERIC.getChars());
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with just upper case letters in the character set.
+	 */
+	CAPITAL {
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(UPPER_ALPHA.getChars());
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with lower case letters and numbers in the character set.
+	 */
+	LETTER_NUMBER {
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(concat(LOWER_ALPHA, NUMERIC));
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with upper and lower case letters in the character set.
+	 */
+	LETTER_CAPITAL {
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(MIXED_ALPHA.getChars());
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with upper case letters and numbers in the character set.
+	 */
+	CAPITAL_NUMBER {
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(concat(NUMERIC, UPPER_ALPHA));
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with numbers, upper and lower case letters in the character set.
+	 */
+	LETTER_CAPITAL_NUMBER {
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(MIXED_ALPHA_NUMERIC.getChars());
+		}
+	},
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with lower case letters, numbers and special characters in the character set.
+	 */
+	LETTER_NUMBER_PUNCTUATION {
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public PasswordGenerator getPasswordGenerator() {
+			return new PasswordGeneratorImpl(concat(LOWER_ALPHA, NUMERIC, SPECIAL));
+		}
+	};
+
+	/**
+	 * Returns a new instance of {@link PasswordGeneratorImpl} with a custom character set.
+	 * 
+	 * @param chars
+	 *            the main body of characters for use in the password generator.
+	 * @param extraChars
+	 *            any extra individual characters to add into the character set.
+	 * 
+	 * @return a new instance of the custom PasswordGenerator.
+	 */
+	public static PasswordGenerator getCustomPasswordGenerator(final char[] chars, final char... extraChars) {
+		return new PasswordGeneratorImpl(add(chars, extraChars));
+	}
+
+	/**
+	 * Returns a new instance of {@link PasswordGenerator}.
+	 * 
+	 * @return a new instance of the PasswordGenerator.
+	 */
+	public abstract PasswordGenerator getPasswordGenerator();
 }
--- password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/PasswordGeneratorProperty.java
+++ password-storage-common/src/main/java/com/jbacon/passwordstorage/password/generation/PasswordGeneratorProperty.java
@@ -1,24 +1,24 @@
 package com.jbacon.passwordstorage.password.generation;
 
 public class PasswordGeneratorProperty {
-    
-    static final String UseCharSet = "Character-Set-Property";
-    
-    static final String PasswordLength = "Password-Length-Property";
-    
-    static final PasswordGeneratorProperty useCharSet(final Character[] charSet) {
-        return new PasswordGeneratorProperty(UseCharSet, charSet);
-    }
-    
-    static final PasswordGeneratorProperty usePasswordLength(final Integer length) {
-        return new PasswordGeneratorProperty(PasswordLength, length);
-    }
-    
-    public final String name;
-    public final Object value;
-    
-    protected PasswordGeneratorProperty(final String name, final Object value) {
-        this.name = name;
-        this.value = value;
-    }
+
+	public static final String UseCharSet = "Character-Set-Property";
+
+	public static final String PasswordLength = "Password-Length-Property";
+
+	public static final PasswordGeneratorProperty useCharSet(final Character[] charSet) {
+		return new PasswordGeneratorProperty(UseCharSet, charSet);
+	}
+
+	public static final PasswordGeneratorProperty usePasswordLength(final Integer length) {
+		return new PasswordGeneratorProperty(PasswordLength, length);
+	}
+
+	public final String name;
+	public final Object value;
+
+	protected PasswordGeneratorProperty(final String name, final Object value) {
+		this.name = name;
+		this.value = value;
+	}
 }
