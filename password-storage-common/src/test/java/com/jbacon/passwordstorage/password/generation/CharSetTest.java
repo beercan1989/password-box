213c12bca5c1906054ef7c276d9c562fb513ce6e
James Bacon <james.bacon@technophobia.com>
2012-06-12 12:28:40 +0100

    Fixed CharSet tests and added logging xml files for tests.

--- password-storage-common/src/test/java/com/jbacon/passwordstorage/password/generation/CharSetTest.java
+++ password-storage-common/src/test/java/com/jbacon/passwordstorage/password/generation/CharSetTest.java
@@ -14,103 +14,115 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
 
 public class CharSetTest {
-    
-    @Test
-    public void shouldBeAbleToAddOneOrMoreArraysIntoOne() {
-        final Character[] result = CharSet.add(LOWER_ALPHA.get(), UPPER_ALPHA.get(), NUMERIC.get());
-        assertThat(result, is(not(nullValue())));
-        assertThat(result.length, is(equalTo(62)));
-    };
-    
-    @Test
-    public void shouldBeAbleToConcatOneOrMoreCharSet() {
-        final Character[] result = CharSet.concat(LOWER_ALPHA, UPPER_ALPHA, NUMERIC);
-        
-        assertThat(result, is(not(nullValue())));
-        assertThat(result.length, is(equalTo(62)));
-    };
-    
-    @Test
-    public void shouldBeAbleToGetCharacterArrayForCharSet() {
-        final Character[] result = LOWER_ALPHA.get();
-        
-        assertThat(result, is(not(nullValue())));
-        assertThat(result, is(instanceOf(Character[].class)));
-        assertThat(result.length, is(greaterThan(0)));
-    };
-    
-    @Test
-    public void shouldHaveLowerAlphaChars() {
-        final Character[] result = LOWER_ALPHA.get();
-        assertThat(result.length, is(equalTo(26)));
-        assertThat(Arrays.asList(result), containsInAnyOrder('a', 'b', 'c', 'd', 'e', 'f', //
-                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
-                'v', 'w', 'x', 'y', 'z'));
-        
-        final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
-        assertThat(resultSet, is(not(nullValue())));
-        assertThat(resultSet.size(), is(equalTo(26)));
-    };
-    
-    @Test
-    public void shouldHaveMixedAlphaChars() {
-        final Character[] result = MIXED_ALPHA.get();
-        assertThat(result.length, is(equalTo(52)));
-        assertThat(Arrays.asList(result), containsInAnyOrder('a', 'b', 'c', 'd', 'e', 'f', //
-                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
-                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', //
-                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
-        
-        final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
-        assertThat(resultSet, is(not(nullValue())));
-        assertThat(resultSet.size(), is(equalTo(52)));
-    };
-    
-    @Test
-    public void shouldHaveMixedAlphaNumericChars() {
-        final Character[] result = MIXED_ALPHA_NUMERIC.get();
-        assertThat(result.length, is(equalTo(62)));
-        assertThat(Arrays.asList(result), containsInAnyOrder('a', 'b', 'c', 'd', 'e', 'f', //
-                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
-                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', //
-                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', //
-                'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
-        
-        final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
-        assertThat(resultSet, is(not(nullValue())));
-        assertThat(resultSet.size(), is(equalTo(62)));
-    }
-    
-    @Test
-    public void shouldHaveNumericChars() {
-        final Character[] result = NUMERIC.get();
-        assertThat(result.length, is(equalTo(10)));
-        assertThat(Arrays.asList(result), containsInAnyOrder('0', '1', '2', '3', '4', '5', //
-                '6', '7', '8', '9'));
-        
-        final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
-        assertThat(resultSet, is(not(nullValue())));
-        assertThat(resultSet.size(), is(equalTo(10)));
-    }
-    
-    @Test
-    public void shouldHaveUpperAlphaChars() {
-        final Character[] result = UPPER_ALPHA.get();
-        assertThat(result.length, is(equalTo(26)));
-        assertThat(Arrays.asList(result), containsInAnyOrder('A', 'B', 'C', 'D', 'E', 'F', //
-                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', //
-                'V', 'W', 'X', 'Y', 'Z'));
-        
-        final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
-        assertThat(resultSet, is(not(nullValue())));
-        assertThat(resultSet.size(), is(equalTo(26)));
-    }
-    
+
+	private static Character[] toCharacterArray(final char[] chars) {
+		final List<Character> result = new ArrayList<Character>();
+
+		for (char character : chars) {
+			result.add(character);
+		}
+
+		return result.toArray(new Character[0]);
+	};
+
+	@Test
+	public void shouldBeAbleToAddOneOrMoreArraysIntoOne() {
+		final Character[] result = toCharacterArray(CharSet.add(LOWER_ALPHA.getChars(), UPPER_ALPHA.getChars(), NUMERIC.getChars()));
+		assertThat(result, is(not(nullValue())));
+		assertThat(result.length, is(equalTo(62)));
+	};
+
+	@Test
+	public void shouldBeAbleToConcatOneOrMoreCharSet() {
+		final Character[] result = toCharacterArray(CharSet.concat(LOWER_ALPHA, UPPER_ALPHA, NUMERIC));
+
+		assertThat(result, is(not(nullValue())));
+		assertThat(result.length, is(equalTo(62)));
+	};
+
+	@Test
+	public void shouldBeAbleToGetCharacterArrayForCharSet() {
+		final Character[] result = toCharacterArray(LOWER_ALPHA.getChars());
+
+		assertThat(result, is(not(nullValue())));
+		assertThat(result, is(instanceOf(Character[].class)));
+		assertThat(result.length, is(greaterThan(0)));
+	};
+
+	@Test
+	public void shouldHaveLowerAlphaChars() {
+		final Character[] result = toCharacterArray(LOWER_ALPHA.getChars());
+		assertThat(result.length, is(equalTo(26)));
+		assertThat(Arrays.asList(result), containsInAnyOrder('a', 'b', 'c', 'd', 'e', 'f', //
+				'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
+				'v', 'w', 'x', 'y', 'z'));
+
+		final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
+		assertThat(resultSet, is(not(nullValue())));
+		assertThat(resultSet.size(), is(equalTo(26)));
+	}
+
+	@Test
+	public void shouldHaveMixedAlphaChars() {
+		final Character[] result = toCharacterArray(MIXED_ALPHA.getChars());
+		assertThat(result.length, is(equalTo(52)));
+		assertThat(Arrays.asList(result), containsInAnyOrder('a', 'b', 'c', 'd', 'e', 'f', //
+				'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
+				'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', //
+				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+
+		final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
+		assertThat(resultSet, is(not(nullValue())));
+		assertThat(resultSet.size(), is(equalTo(52)));
+	};
+
+	@Test
+	public void shouldHaveMixedAlphaNumericChars() {
+		final Character[] result = toCharacterArray(MIXED_ALPHA_NUMERIC.getChars());
+		assertThat(result.length, is(equalTo(62)));
+		assertThat(Arrays.asList(result), containsInAnyOrder('a', 'b', 'c', 'd', 'e', 'f', //
+				'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
+				'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', //
+				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', //
+				'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+
+		final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
+		assertThat(resultSet, is(not(nullValue())));
+		assertThat(resultSet.size(), is(equalTo(62)));
+	}
+
+	@Test
+	public void shouldHaveNumericChars() {
+		final Character[] result = toCharacterArray(NUMERIC.getChars());
+		assertThat(result.length, is(equalTo(10)));
+		assertThat(Arrays.asList(result), containsInAnyOrder('0', '1', '2', '3', '4', '5', //
+				'6', '7', '8', '9'));
+
+		final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
+		assertThat(resultSet, is(not(nullValue())));
+		assertThat(resultSet.size(), is(equalTo(10)));
+	}
+
+	@Test
+	public void shouldHaveUpperAlphaChars() {
+		final Character[] result = toCharacterArray(UPPER_ALPHA.getChars());
+		assertThat(result.length, is(equalTo(26)));
+		assertThat(Arrays.asList(result), containsInAnyOrder('A', 'B', 'C', 'D', 'E', 'F', //
+				'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', //
+				'V', 'W', 'X', 'Y', 'Z'));
+
+		final Set<Character> resultSet = new HashSet<Character>(Arrays.asList(result));
+		assertThat(resultSet, is(not(nullValue())));
+		assertThat(resultSet.size(), is(equalTo(26)));
+	}
+
 }
